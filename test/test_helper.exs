4ea12112cdf706eec0b848c47c1cb1d5027fd284
Keith Brings <keith.brings@noizu.com>
2019-03-10 12:27:13 +0700

    mix.lock and test helper tweak for semaphore calls

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,11 @@
+# Needed for tzdata
+IO.puts "Create ets table"
+:ets.new(:semaphore, [:set, :public, :named_table, {:write_concurrency, true}]) |> IO.inspect
+
 # Ensure tzdata is up to date
 {:ok, _} = Application.ensure_all_started(:tzdata)
 _ = Tzdata.ReleaseUpdater.poll_for_update()
+
+
 ExUnit.configure(exclude: [skip: true])
 ExUnit.start()
